5879dc4b9aeaeff1d9773c734f1a17cef20b1cd9
Francois le Roux <francoislerouxdev@gmail.com>
2026-08-08 13:11:31 +0200

    Part 1 done
    Part 1 to the CRUD tutorial is completed

---
+++ pet-registry/App.js
@@ -0,0 +1,10 @@
+
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems:"center" }}>
+      <Text style={{fontize: 22 }}> Bikini Bottom Pet Registry </Text>
+    </View>
+  );
+}
